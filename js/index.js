9388895d361a42eb2949f6e369303ece543b69fd
 <seiji.mnsm@gmail.com>
2024-01-05 23:49:25 +0900

    諸々修正

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   // モーダルボタンの要素を取得
-  var modalButton = document.querySelector('.js-modal-button');
+  const modalButton = document.querySelector('.js-modal-button');
 
   // モーダルを開閉する関数
   function toggleModal() {
     // グレー背景の要素を取得
-    var modalLayer = document.querySelector('.js-modal');
+    const modalLayer = document.querySelector('.js-modal');
 
     // グレー背景にis-showクラスをトグルして表示/非表示を切り替える
     modalLayer.classList.toggle('is-show');
@@ -22,13 +22,13 @@ document.addEventListener('DOMContentLoaded', function () {
   modalButton.addEventListener('click', toggleModal);
 
   // ×ボタンの要素を取得
-  var closeButton = document.querySelector('.js-close-button');
+  const closeButton = document.querySelector('.js-close-button');
 
   // ×ボタンがクリックされたときにtoggleModal関数を実行
   closeButton.addEventListener('click', toggleModal);
 
   // グレー背景の要素を取得 同一コードがあるが、以下がないとグレー背景をクリックして反応しなくなる。
-  var modalLayer = document.querySelector('.js-modal');
+  const modalLayer = document.querySelector('.js-modal');
 
   // グレー背景がクリックされたときにtoggleModal関数を実行
   modalLayer.addEventListener('click', function (event) {
