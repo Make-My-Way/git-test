a2af04fcd50c169085b788682b61a90e92940e23
 <seiji.mnsm@gmail.com>
2024-01-17 16:20:30 +0900

    色々

--- js/index.js
+++ js/index.js
@@ -1,36 +1,28 @@
-// スムーズスクロール
-const headerHeight = document.querySelector('header').offsetHeight;
+const mySwiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+ 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const href = anchor.getAttribute('href');
-        const target = document.getElementById(href.replace('#', ''));
-        if (target) {
-            const targetPosition = target.getBoundingClientRect().top + window.scrollY - headerHeight;
-            window.scrollTo({
-                top: targetPosition,
-                behavior: 'smooth'
-            });
-        }
-    });
+  speed: 1000,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+
+ 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ 
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
 
-// ページ外スクロール
-window.addEventListener('load', function () {
-    const url = new URL(location.href);
-    const hash = url.hash.slice(1);
-    const target = document.getElementById(hash);
-    if (target) {
-        const top = target.getBoundingClientRect().top + window.scrollY - headerHeight;
-        setTimeout(function () {
-            window.scrollTo({ top: 0 }, 0);
-        });
-        setTimeout(function () {
-            window.scrollTo({
-                top: top,
-                behavior: 'smooth'
-            });
-        });
-    }
-});
